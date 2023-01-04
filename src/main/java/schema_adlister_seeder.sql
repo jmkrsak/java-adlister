3a438cbd10cefc378c246c96e2d283b7c975b8ba
John-Michael Krsak <jmkrsak@gmail.com>
2023-01-04 13:42:11 -0500

    update

---
+++ src/main/java/schema_adlister_seeder.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS adlister_db;
+CREATE DATABASE adlister_db;
+USE adlister_db;
+CREATE USER IF NOT EXISTS 'master'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL ON adlister_db.* To 'master'@'localhost';
+CREATE TABLE users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
