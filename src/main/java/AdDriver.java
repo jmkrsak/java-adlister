1b73c5c5f4bea71cb6e9f552d259fd86dfdece17
John-Michael Krsak <jmkrsak@gmail.com>
2023-01-04 17:48:06 -0500

    update

---
+++ src/main/java/AdDriver.java
@@ -0,0 +1,25 @@
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class AdDriver {
+
+    public static void main(String[] args) throws SQLException {
+
+        DriverManager.registerDriver(new Driver());
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false",
+                "root",
+                "codeup");
+
+        Statement statement = connection.createStatement();
+
+        statement.execute("INSERT INTO users (username, email, password) VALUES ('JohnM','@JohnM', '1234')");
+
+    }
+
+}
--- src/main/java/schema_adlister_seeder.sql
+++ src/main/java/schema_adlister_seeder.sql
@@ -15,7 +15,7 @@ CREATE TABLE ads (
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
